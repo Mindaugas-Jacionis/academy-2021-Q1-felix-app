3e91e3762d256c59cde5c494399e356074d97829
Mindaugas Jačionis <m.jacionis@gmail.com>
2021-01-21 20:52:35 +0200

    Initial project structure example

---
+++ src/app/components/ExampleButton/index.js
@@ -0,0 +1,7 @@
+import "./index.scss";
+
+function Button({ children }) {
+  return <button className="Button">{children}</button>;
+}
+
+export default Button;
---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+import { Button } from "./components";
+import "./index.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Here goes our app</h1>
+      <Button>Example Button</Button>
+    </div>
+  );
+}
+
+export default App;
